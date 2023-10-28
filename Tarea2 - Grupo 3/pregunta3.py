b14d32e656c61f474c5efa1e6dd44d36bb87ba5b
olman2020 <irodriguezcr1@gmail.com>
2023-10-27 20:08:06 -0600

    Versión totalmente corregida

--- Tarea2 - Grupo 3/pregunta3.py
+++ Tarea2 - Grupo 3/pregunta3.py
@@ -1,37 +1,41 @@
 import numpy as np
 
-def MHSS(A, x0, iter_max, tol):
-    m = len(x0)
-    Im = np.eye(m, dtype=complex)
+def mhss(A, b, x0, max_iter=1000, tol=1e-12):
+    n = A.shape[0]
     x = x0
 
-    W, T = A
-    alpha_star = np.sqrt(np.min(np.linalg.eigvals(W)) * np.max(np.linalg.eigvals(W)))
-
-
-    for k in range(iter_max):
-        M = np.linalg.inv(alpha_star * Im + T).dot((alpha_star * Im + 1j * W).dot(np.linalg.inv(alpha_star * Im + W).dot(alpha_star * Im - 1j * T)))
-        N = (1 - 1j) * alpha_star * np.linalg.inv(alpha_star * Im + T).dot(np.linalg.inv(alpha_star * Im + W))
-
-        x_new = M.dot(x) + N.dot(A[1])
-        if np.linalg.norm(A[0].dot(x_new) - x0) < tol:
-            return x_new
-        x = x_new
-
+    for k in range(max_iter):
+        r = b - np.dot(A, x)
+        z = np.linalg.solve(A + np.conj(A.T), r)
+        x = x + z
+        
+        if np.linalg.norm(np.dot(A, x) - b) < tol:
+            return x
+    
     return x
 
-if __name__ == "__main__":
-    W = np.array([[12, -2, 6, -2], [-2, 5, 2, 1], [6, 2, 9, -2], [-2, 1, -2, 1]], dtype=complex)
-    T = np.array([[6, 2, 7, 2], [2, 7, 1, 1], [7, 1, 9, 0], [2, 1, 0, 10]], dtype=complex)
-    p = np.array([[9], [-7], [-5], [7]], dtype=complex)
-    q = np.array([[12], [-4], [17], [-2]], dtype=complex)
-    A = (W + 1j * T, p + 1j * q)
-    x0 = np.array([[1], [-1], [0], [-0]], dtype=complex)
-    iter_max = 1000
-    tol = 1e-12
+def calculate_optimal_alpha(W):
+    eigenvalues_W = np.linalg.eigvals(W)
+    alpha_star = np.sqrt(np.min(eigenvalues_W) * np.max(eigenvalues_W))
+    return alpha_star
 
-    solution = MHSS(A, x0, iter_max, tol)
+W = np.array([[12, -2, 6, -2], [-2, 5, 2, 1], [6, 2, 9, -2], [-2, 1, -2, 1]], dtype=complex)
+T = np.array([[6, 2, 7, 2], [2, 7, 1, 1], [7, 1, 9, 0], [2, 1, 0, 10]], dtype=complex)
+p = np.array([9, -7, -5, 7], dtype=complex)
+q = np.array([12, -4, 17, -2], dtype=complex)
 
-    print("Solución encontrada:")
-    print(solution)
-    
+A = W + 1j * T
+b = p + 1j * q
+x0 = np.array([1, -1, 1j, -1j], dtype=complex)
+iter_max = 1000
+tol = 1e-12
+solution = mhss(A, b, x0, iter_max, tol)
+
+print("Solución encontrada:")
+print(solution)
+print("Solución real:")
+real_solution = np.array([1, -1, 1j, -1j], dtype=complex)
+print(real_solution)
+
+alpha_star = calculate_optimal_alpha(W)
+print("Valor óptimo de alpha*:", alpha_star)
